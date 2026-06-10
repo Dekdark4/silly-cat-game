022f315e36e3abc58dff58d022762cc082cbbda9
Dekdark4 <zzkskl@gmail.com>
2026-06-10 18:58:34 +0300

    Разобрался как сделать fullscreen. Я смог. Доделано!

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Silly Cat", sf::State::Fullscreen);
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
