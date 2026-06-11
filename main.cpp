0afc8c4e7f94a41d561a4a65794f6d8b6c8caf4e
Dekdark4 <zzkskl@gmail.com>
2026-06-11 16:14:36 +0300

    Баловался с движением и коллизией.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Silly Cat", sf::State::Fullscreen);
     sf::CircleShape shape(100.f);
+    sf::CircleShape enemy(100.f);
+    enemy.setFillColor(sf::Color::Red);
+    enemy.setPosition({ 1500.f, 0.f });
+
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
@@ -19,9 +23,21 @@ int main()
             }
         }
 
+        sf::Vector2f playerPos = shape.getPosition();
+        sf::Vector2f enemyPos = enemy.getPosition();
+        float dx = playerPos.x - enemyPos.x;
+        float dy = playerPos.y - enemyPos.y;
+        float distance = std::sqrt(dx * dx + dy * dy);
+        if (distance < shape.getRadius() + enemy.getRadius()) {
+            // Столкновение!
+            shape.setFillColor(sf::Color::Yellow); // Игрок пожелтел от укуса
+        }
+        enemy.move({-0.1f, 0.f});
+
 
         window.clear();
         window.draw(shape);
+        window.draw(enemy);
         window.display();
     }
 }
