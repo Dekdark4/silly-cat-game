0c6d18742d587fbe4893e80168a460f252f01733
Dekdark4 <zzkskl@gmail.com>
2026-06-11 06:50:53 +0300

    Добавил половину конструктора для Entity.

--- Entity.cpp
+++ Entity.cpp
@@ -1,1 +1,7 @@
 ﻿#include "Entity.h"
+
+Entity::Entity::Entity(float startX, float startY, float radius) : shape{ radius }
+{
+	//shape.setPosition(startX, startY);
+	shape.setFillColor(sf::Color::White);
+}
--- Entity.h
+++ Entity.h
@@ -1,11 +1,17 @@
 ﻿#pragma once
+#include <SFML/Graphics.hpp>
 
 namespace Entity
 {
 	class Entity
 	{
 	public:
-	private:
+		Entity(float startX, float startY, float radius);
+
+		virtual void Update(float deltaTime) = 0;
+		void Move(float offsetX, float offsetY);
+	protected:
+		sf::CircleShape shape{};
 	};
 
 	class Player
